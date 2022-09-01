ce78da830ee3a4341561bba5a964232e3f5f4666
fridah karimi <fridah.karimi@student.moringaschool.com>
2022-09-01 22:57:53 +0300

    select the required cotainers

---
+++ js/script.js
@@ -0,0 +1,4 @@
+  let gridContainer = document.getElementById("category_container");
+  let riddleContainer = document.getElementById("riddle");
+  let answer = document.getElementById("riddle_answer");
+
