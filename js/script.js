4046e16de05d5c76beccc20062d07c5686d238bb
fridah karimi <fridah.karimi@student.moringaschool.com>
2022-09-01 22:58:13 +0300

    store the required API urls

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+  const URL = "https://www.themealdb.com/api/json/v1/1/categories.php";
+  const riddleURL = "https://api.api-ninjas.com/v1/riddles";
   let gridContainer = document.getElementById("category_container");
   let riddleContainer = document.getElementById("riddle");
   let answer = document.getElementById("riddle_answer");
